ee685616d2cd14e09131170aea8f056cd002e7d2
Andrey Gomes <andreygdsn@gmail.com>
2025-07-15 18:00:01 -0300

    feat: basic structure for consume api

---
+++ frontend/src/app/pages/dishes/dish-form/dish-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-form',
+  imports: [],
+  templateUrl: './dish-form.html',
+  styleUrl: './dish-form.scss'
+})
+export class DishForm {
+
+}
