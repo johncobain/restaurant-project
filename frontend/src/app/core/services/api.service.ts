6a109a49f7b590957f2b678de30451db017a096f
Andrey Gomes <andreygdsn@gmail.com>
2025-07-15 23:14:59 -0300

    feat: base service

---
+++ frontend/src/app/core/services/api.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  get<T>(endpoint: string): Observable<T> {
+    return this.http
+      .get<T>(`${this.baseUrl}${endpoint}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  post<T>(endpoint: string, data: any): Observable<T> {
+    return this.http
+      .post<T>(`${this.baseUrl}${endpoint}`, data)
+      .pipe(catchError(this.handleError));
+  }
+
+  put<T>(endpoint: string, data: any): Observable<T> {
+    return this.http
+      .put<T>(`${this.baseUrl}${endpoint}`, data)
+      .pipe(catchError(this.handleError));
+  }
+
+  delete<T>(endpoint: string): Observable<T> {
+    return this.http
+      .delete<T>(`${this.baseUrl}${endpoint}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('API Error:', error);
+    return throwError(() => error);
+  }
+}
